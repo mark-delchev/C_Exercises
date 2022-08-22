b7e206f0d5be16ac5c2730ec07fa4a8b5030e5eb
Mark Delchev <markdelchev@gmail.com>
2022-08-22 15:43:39 +0300

    apples_calculator

---
+++ Basic_Exercises/apples_calculator.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int tree_one = 150;
+    int tree_two = 142;
+    int tree_three = 127;
+    int total = tree_one + tree_two + tree_three;
+    printf("Apples gathered = %d\n", total);
+    /*printf("You are not allowed to cheat, or are you…?");*/
+    return 0;
+}
