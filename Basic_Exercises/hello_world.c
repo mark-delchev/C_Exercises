22495ff5ff2906c80bd592f246afa6361cce22d8
Mark Delchev <markdelchev@gmail.com>
2022-08-22 15:33:12 +0300

    write_your_name

--- Basic_Exercises/hello_world.c
+++ Basic_Exercises/hello_world.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 int main()
 {
-    int num = 5;
-
     printf("Hello SoftUni!\n");
     return 0;
 }
---
+++ Basic_Exercises/write_your_name.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    char your_name[60];
+    scanf("%s", your_name);
+    printf("My name is %s", your_name);
+    return 0;
+}
