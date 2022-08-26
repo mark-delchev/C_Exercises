231f0e2ece0d39c15b777c85eb66be600bc24af3
Mark Delchev <markdelchev@gmail.com>
2022-08-26 21:54:19 +0300

    info.c and numbers.c update

---
+++ Basic_Exercises/info.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("Name   : Mark Delchev\n");
+    printf("DOB    : June 16, 2004\n");
+    printf("Mobile : 99-9999999999\n");
+    return 0;
+}
--- Basic_Exercises/numbers.c
+++ Basic_Exercises/numbers.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int num1;
-    int num2;
-    printf("First number: ");
+    int num1, num2, num3;
+    printf("\nFirst number: ");
     scanf("%d", &num1);
-    printf("Second number: ");
+    printf("\nSecond number: ");
     scanf("%d", &num2);
-    int num3 = num1 + num2;
-    printf("The first number plus the second is equal to: %d", num3);
+    num3 = num1 + num2;
+    printf("\nThe first number plus the second is equal to: %d", num3);
     return 0;
 }
