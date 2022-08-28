16c0df9efef7e0114d774f809d3a8c81c0870e7c
Mark Delchev <markdelchev@gmail.com>
2022-08-28 17:56:23 +0300

    more data types

--- Basic_Exercises/data_types.c
+++ Basic_Exercises/data_types.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <stdbool.h>
 int main()
 {
-    char name[] = "Ivan";
-    char letter = 'B';
-    float grade = 2.5;
-    printf("%s%c, your grade is: %f", name, letter, grade);
+    int num = 1234;
+    long long numm = 9223372036854775807;  // %lld to display
+    char f = 120; // %d to display and %c ASCII
+    unsigned long long num3 = 18446744073709551615U; // %llu to display
+    unsigned int num2 = 123333; // %u to sidplay
+    char name[] = "Ivan";// %s to display
+    char letter = 'B';// %c to display
+    float grade = 2.5; // %f to display
+    double grade2 = 2.5; // more accurate than float %lf to display
+    bool e = true;
+    printf("%s%c, your grade is: %0.2f\n", name, letter, grade); // use 0.x to determine the number of digits displayed
+    printf("%llu", num3);
     return 0;
 }
