813a99cee19f200b0f2f0bc451227449c42eb9d7
Mark Delchev <markdelchev@gmail.com>
2022-08-28 17:30:42 +0300

    data_types.c

---
+++ Basic_Exercises/data_types.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    char name[] = "Ivan";
+    char letter = 'B';
+    float grade = 2.5;
+    printf("%s%c, your grade is: %f", name, letter, grade);
+    return 0;
+}
